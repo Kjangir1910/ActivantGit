1ecfe07a38057256412891a21ec6834487dc8ba9
Kjangir1910 <129332297+Kjangir1910@users.noreply.github.com>
2024-09-18 02:14:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -57,6 +57,46 @@ app.get('/api/users', async (req, res) => {
     }
 })
 
+app.delete('/api/users/:id', async (req, res) => {
+    try {
+        const {id} = req.params
+        await User.findByIdAndDelete(id)
+        res.status(200).json({message: 'user deleted successfully'})
+    } catch (error) {
+        res.status(500).json({error: 'unable to delete user'})
+    }
+})
+
+// Delete user
+// app.delete('/api/users/:id', async (req, res) => {
+//     const { id } = req.params; 
+//     try {
+//       const deletedUser = await User.findByIdAndDelete(id); 
+//       if (!deletedUser) {
+//         return res.status(404).json({ error: 'User not found' }); 
+//       }
+//       res.status(200).json({ message: 'User deleted successfully' }); 
+//     } catch (error) {
+//       res.status(500).json({ error: 'Unable to delete user' }); 
+//     }
+//   });
+
+// Update user
+app.put('/api/users/;id', async (req, res) => {
+    const {id} = req.params
+    const {name, email, address} = req.body
+    try {
+        const updatedUser = await User.findByIdAndUpdate(id, {name, email, address}, {new: true}) 
+        if(!updatedUser) {
+            return res.status(404).json({error: 'User not found'})
+        }
+        res.status(200).json(updatedUser)
+    } catch (error) {
+        res.status(500).json({error: 'Unable to update user'})
+    }
+})
+  
+
 // Server
 const PORT = process.env.port || 5000
 app.listen(PORT, () => {
